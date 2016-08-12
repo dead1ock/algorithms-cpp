8d2e341ee38ccc514b1836783195e6d62fd929d8
Dallin Wellington <dallin.wellington@gmail.com>
2016-08-12 13:59:11 -0700

    Implemented an insertion sort algorithm. Created unit test to verify insertion sort's correctness.

--- src/sort/sort.h
+++ src/sort/sort.h
@@ -20,3 +20,24 @@ void SelectionSort(T* in, unsigned int size) {
 		*smallest = in[x];
 	}
 }
+
+/*
+ * Runtime Analysis: O(N^2)
+ */
+template<typename T>
+void InsertionSort(T* in, unsigned int size) {
+	for (int x = 0; x < size; x++)
+	{
+		for (int y = x; y > 0; y--)
+		{
+			if (in[x] < in[y])
+			{
+				T smaller = in[x];
+				in[y] = in[x];
+				in[x] = smaller;
+			}
+			else
+				break;
+		}
+	}
+}
--- tests/sort.cpp
+++ tests/sort.cpp
@@ -22,3 +22,24 @@ TEST(SelectionSort, Sort10)
 	EXPECT_EQ(8, collection[8]);
 	EXPECT_EQ(9, collection[9]);
 }
+
+TEST(InsertionSort, Sort10)
+{
+	int collection[10];
+
+	for (int x = 9; x >= 0; x--)
+		collection[x] = x;
+
+	InsertionSort<int>(collection, 10);
+
+	EXPECT_EQ(0, collection[0]);
+	EXPECT_EQ(1, collection[1]);
+	EXPECT_EQ(2, collection[2]);
+	EXPECT_EQ(3, collection[3]);
+	EXPECT_EQ(4, collection[4]);
+	EXPECT_EQ(5, collection[5]);
+	EXPECT_EQ(6, collection[6]);
+	EXPECT_EQ(7, collection[7]);
+	EXPECT_EQ(8, collection[8]);
+	EXPECT_EQ(9, collection[9]);
+}
